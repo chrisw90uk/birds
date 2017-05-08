9f58fe44aabd6a38c9c0963adb8bdbd1f447144d
Chris Wickham <chrisw90_uk@hotmail.com>
2017-05-08 22:03:27 +0100

    camera locked on player, grass sprite test

---
+++ Assets/camCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camCtrl : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;	
+	}
+	
+	// Update is called once per frame after all player actions
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
